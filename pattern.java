84f86e6564f7680babf9ffdd708f84291ed9d86a
Pooja123113 <85823029+Pooja123113@users.noreply.github.com>
2021-07-14 14:09:44 +0530

    Add files via upload
    Pattern program on java.

---
+++ pattern.java
@@ -0,0 +1,16 @@
+class pattern4 
+{
+	public static void main(String[]args)
+{
+	int i,j,n=10;
+	for(i=n-1;i>=0;i--)
+	{
+	
+	for(j=0;j<=i;j++)
+	{
+	System.out.print("*" + " ");
+	}
+	System.out.println();
+   }
+  }
+}
